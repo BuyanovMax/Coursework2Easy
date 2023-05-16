73a49a002558abf5648c345fadbf0049ad2ef6a9
Макс <maxim__90.90@mail.ru>
2023-05-16 17:55:15 +0400

    first commit

---
+++ src/main/java/com/example/coursework2easy/Coursework2EasyApplication.java
@@ -0,0 +1,13 @@
+package com.example.coursework2easy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Coursework2EasyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Coursework2EasyApplication.class, args);
+    }
+
+}
